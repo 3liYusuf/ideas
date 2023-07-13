e3de85e7c3908f7f6936a3bb867a252665ddfbe8
Ali Yusuf <alcndb@gmail.com>
2023-07-13 15:59:53 +0300

    initial idea project

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const app = express();
 const mongoose = require("mongoose");
 const { log } = require("console");
-const encrypt = require('mongoose-encryption');
-var session = require('express-session');
 const passport = require("passport");
-const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 
@@ -15,166 +11,65 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:true}));
 
-app.use(session({
-    secret: "Our little secret.", //to encrypt session data
-    resave: false, //true=save session every request, false=optimize perfromance & save when needed
-    saveUninitialized: false, //true=create session for every user, false=session for initialized
-}))
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser:true});
+mongoose.connect("mongodb+srv://admin-ali:Test123@cluster0.i5q0kly.mongodb.net/ideas", {useNewUrlParser:true});
 
-const userSchema = new mongoose.Schema({
-    email:String,
-    password:String,
-    googleId:String
+const ideaSchema = new mongoose.Schema({
+    name:String,
+    description:String,
+    job:String,
+    type:String,
+    lang:String
 });
 
-const secretSchema = new mongoose.Schema({
-    text:String
-});
+ideaSchema.plugin(findOrCreate);
 
-const Secret = mongoose.model("Secret", secretSchema);
+const Idea = mongoose.model("Idea", ideaSchema);
 
-userSchema.plugin(passportLocalMongoose);
-userSchema.plugin(findOrCreate);
-
-const User = mongoose.model("User", userSchema);
-
-passport.use(User.createStrategy());
-passport.serializeUser(function(user, cb) {
-    process.nextTick(function() {
-        return cb(null, {
-        id: user.id,
-        username: user.username,
-        picture: user.picture
-        });
-    });
-});
-  
-passport.deserializeUser(function(user, cb) {
-process.nextTick(function() {
-    return cb(null, user);
-});
-});
-passport.use(new GoogleStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
-    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    log(profile);
-    User.findOrCreate({username: profile.displayName, googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
+// const Website = mongoose.model("Website", ideaSchema);
+// const Application = mongoose.model("Application", ideaSchema);
+// const Logo = mongoose.model("Logo", ideaSchema);
+// const SocialMedia = mongoose.model("SocialMedia", ideaSchema);
+// const VisualIdentity = mongoose.model("VisualIdentity", ideaSchema);
+// const Prints = mongoose.model("Prints", ideaSchema);
+// const Ui = mongoose.model("Ui", ideaSchema);
 
 
 app.get("/", function(req,res){
     res.render("home");
 })
 
-app.get("/auth/google", passport.authenticate('google', {scope:["profile"]}));
-
-
-app.get('/auth/google/secrets', 
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/secrets');
-  }
-);
-
-app.get("/login", function(req,res){
-    res.render("login");
-})
-
-app.get("/register", function(req,res){
-    res.render("register");
-})
-
-app.get("/submit", function(req,res){
-    if(req.isAuthenticated()){
-        res.render("submit");
-    }else{
-        res.redirect("/login");
-    }
-
-})
-
-app.get("/secrets", function(req,res){
-    if(req.isAuthenticated()){
-        Secret.find(function(err,foundSecrets){
-            if(!err){
-                res.render("secrets",{foundSecrets:foundSecrets});
-            }
-        })
-    }else{
-        res.redirect("/login");
-    }
-})
-
-app.get("/logout", function(req,res){
-    req.logout((function(err) {
-        if (err) {
-            log(err);
+app.post("/idea", function(req,res){
+    Idea.findOne({type:req.body.category_id},function(err,found){
+        if(found){
+            res.render("idea",{name:found.name,description:found.description,job:found.job,type:found.type,selected:req.body.category_id});
         }else{
-        res.redirect('/');}
-      }));
-})
-
-app.post('/register',function(req,res){
-
-    User.register({username:req.body.username}, req.body.password, function(err,user){
-        if(err){
-            log(err);
-            res.redirect("/register");
-        }else{
-            passport.authenticate("local")(req,res,function(){
-                res.redirect("/secrets");
-            })
+            res.send("Not found");
         }
     })
+
 })
 
-app.post("/login",function(req,res){
-    const username = req.body.username;
-    const password = req.body.password;
+app.get("/add",function(req,res){
+    res.render("add");
+})
 
-    const user = new User({
-        username: username,
-        password: password
+app.get("/idea",function(req,res){
+    res.redirect("/");
+})
+
+
+app.post("/add",function(req,res){
+    Idea.create({
+        name: req.body.name,
+        description: req.body.description,
+        job: req.body.job,
+        type: req.body.category_id,
+        lang:"EN"
     });
-
-    req.login(user, function(err){
-        if(err){
-            log(err);
-        }else{
-            passport.authenticate("local")(req,res,function(){
-                res.redirect("/secrets");
-            })
-        }
-    })
+    res.redirect("/add");
 })
-
-app.post("/submit",function(req,res){
-    const insertedSecret = req.body.secret;
-    const secret = new Secret({text:insertedSecret});
-    secret.save(function(err){
-        if(!err){
-            res.redirect("secrets");
-        }else{
-            console.log(err);
-            res.redirect("submit");
-        }
-    });
-})
-
 
 
 
